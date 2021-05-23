c89f7fb9112c9c409b38e5c622ee4e3edd365e59
Tipu-hub1 <74842187+Tipu-hub1@users.noreply.github.com>
2021-05-23 15:52:58 -0400

    Updating a print command

--- Documents/Java_Selenium/Urme/src/classwork/FirstCodewithSelenium.java
+++ Documents/Java_Selenium/Urme/src/classwork/FirstCodewithSelenium.java
@@ -25,7 +25,7 @@ public class FirstCodewithSelenium {
 		
 		driver.navigate().to("http://automationpractice.com/index.php");
 		Thread.sleep(4000);
-		
+		System.out.println("changed from Github by Bashir");
 //		driver.navigate().refresh();
 //		driver.navigate().forward();
 //		driver.navigate().back();
