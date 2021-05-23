18cec9276426d0dd52cb77379ad280cb0186b24b
bashiruddin <bashiruddin@localhost>
2021-05-23 16:32:20 -0400

    changed from local

--- Documents/Java_Selenium/Urme/src/classwork/FirstCodewithSelenium.java
+++ Documents/Java_Selenium/Urme/src/classwork/FirstCodewithSelenium.java
@@ -29,7 +29,8 @@ public class FirstCodewithSelenium {
 		System.out.println("Changed by Laboni");
 		System.out.println("conflict_1 from local");
 		System.out.println("conflict from another coworker");
-
+		
+		System.out.println("Changed from local");
 //		driver.navigate().refresh();
 //		driver.navigate().forward();
 //		driver.navigate().back();
